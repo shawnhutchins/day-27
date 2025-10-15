c5644f09f78d6c088dc309cd3b170890d71d84ae
shawn <shawnhutchins1@gmail.com>
2025-10-14 20:59:32 -0400

    Added a label

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
 window.mainloop()
