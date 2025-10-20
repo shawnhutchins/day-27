89b2d9da0ae8dc303e54c22da30750e4b4543f97
shawn <shawnhutchins1@gmail.com>
2025-10-19 21:35:34 -0400

    Reorganized the code

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import tkinter as tk
 
+def button_clicked():
+    my_label["text"] = text_input.get()
+
 window = tk.Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
 #Label
 my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
 
 #Button
-def button_clicked():
-    my_label["text"] = text_input.get()
-
 button = tk.Button(text="Click Me", command=button_clicked)
-button.pack()
 
 #Entry
-
 text_input = tk.Entry(width=10)
+
+#Packing
+my_label.pack()
+button.pack()
 text_input.pack()
 
 window.mainloop()
